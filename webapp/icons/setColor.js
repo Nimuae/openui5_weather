37964a263443ffda6898acee9e215978208c724f
Dominic Holzwarth <dominic.holzwarth@sap.com>
2015-10-22 15:09:58 +0200

    New SVG Color: #555

--- webapp/icons/setColor.js
+++ webapp/icons/setColor.js
@@ -15,20 +15,24 @@ function changeFile(fileName, newColor){
 }
 
 var color = process.argv.indexOf("-c") !== -1 ? process.argv[process.argv.indexOf("-c") + 1] : null;
-var dir = process.argv.indexOf("-c") !== -1 ? process.argv[process.argv.indexOf("-d") + 1] : null;
+var dir = process.argv.indexOf("-d") !== -1 ? process.argv[process.argv.indexOf("-d") + 1] : null;
+var file = process.argv.indexOf("-f") !== -1 ? process.argv[process.argv.indexOf("-f") + 1] : null;
 
 if(!color){
 	console.log("No new color chosen. Continuing with \"" + defaultColor + "\"...");
 }
 
-if(!dir){
-	console.log("No directory chosen. Stopping now.");
+if(dir){
+	var aFiles = fs.readdirSync(dir);
+	for(var i = 0; i < aFiles.length; i++){
+		if(fs.statSync(dir + "/" + aFiles[i]).isFile()){
+			changeFile(dir + "/" + aFiles[i], color);
+		}
+	}
 	process.exit(0);
 }
 
-var aFiles = fs.readdirSync(dir);
-for(var i = 0; i < aFiles.length; i++){
-	if(fs.statSync(dir + "/" + aFiles[i]).isFile()){
-		changeFile(dir + "/" + aFiles[i], color);
-	}
+if(file){
+	changeFile(file, color);
+	process.exit(0);
 }
