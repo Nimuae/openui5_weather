02aa449dad46b5022ca11ce8552d4cf1d9cb394a
Dominic Holzwarth <dominic.holzwarth@sap.com>
2015-11-19 19:30:52 +0100

    Randomizer Update - Dependencies

--- randomizer.js
+++ randomizer.js
@@ -1,84 +1,120 @@
-var fields = {
-	"temp_c": [-20.0, 40.0], //between -20 and 40
-	"relative_humidity": {
-		range: [0, 99],
-		suffix: "%"
-	},
-	"wind_dir": {
-		select: [
-			"Nord", "Nord-Nordost", "Nordost", "Ost-Nordost", "Ost",
-			"Ost-Südost", "Südost", "Süd-Südost", "Süd",
-			"Süd-Südwest", "Südwest", "Südwest-West", "West",
-			"West-Nordwest", "Nordwest", "Nord-Nordwest"
-		]
-	},
-	"wind_degrees": {
-		range: [0, 360]
-	},
-	"icon": {
-		select: [
-			"chanceflurries", "chancerain",
-			"chancesleet", "chancesnow",
-			"chancetstorms", "clear", "cloudy",
-			"flurries", "fog",
-			"hazy", "mostlycloudy", "mostlysunny",
-			"partlycloudy", "partlysunny",
-			"sleet", "rain", "snow",
-			"sunny", "tstorms",
-			"nt_chanceflurries", "nt_flurries", "nt_chancesnow",
-			"nt_snow", "nt_chancerain",
-			"nt_rain", "nt_chancesleet", "nt_sleet",
-			"nt_chancetstorms", "nt_tstorms",
-			"nt_clear", "nt_sunny", "nt_cloudy",
-			"nt_fog", "nt_hazy",
-			"nt_mostlycloudy", "nt_partlycloudy",
-			"nt_mostlysunny", "nt_partlysunny"
-		]
-	},
-	"wind_kph": [0, 15],
-	"feelslike_c": [-30, 50],
-	"precip_today_metric": [0, 100],
-	"celsius": [-20.0, 40.0]
-};
+module.exports = function(){
+	var that = this;
 
-var rand = function(min, max) {
-	if (!max) {
-		max = min;
-		min = 0;
-	}
-	return min + Math.floor((max - min) * Math.random());
-};
+	this.init = function(){
+		that.stack = {};
+	};
+	this.stack = {};
+	this.fields = {
+		"temp_c": [-20.0, 40.0], //between -20 and 40
+		"relative_humidity": {
+			range: [0, 99],
+			suffix: "%"
+		},
+		"wind_degrees": [0, 360],
+		"wind_dir": {
+			depends: {
+				field: "wind_degrees",
+				fn: function(k, v){
+					var dirs = [
+						"Nord", "Nord-Nordost", "Nordost", "Ost-Nordost", "Ost",
+						"Ost-Südost", "Südost", "Süd-Südost", "Süd",
+						"Süd-Südwest", "Südwest", "Südwest-West", "West",
+						"West-Nordwest", "Nordwest", "Nord-Nordwest"
+					];
+					if(typeof v !== "undefined"){
+						return dirs[Math.min(dirs.length - 1, Math.round(v / Math.floor((360 / dirs.length))))];
+					}else{
+						return dirs[0];
+					}
+					
+				}
+			}
+		},
+		"icon": {
+			select: [
+				"chanceflurries", "chancerain",
+				"chancesleet", "chancesnow",
+				"chancetstorms", "clear", "cloudy",
+				"flurries", "fog",
+				"hazy", "mostlycloudy", "mostlysunny",
+				"partlycloudy", "partlysunny",
+				"sleet", "rain", "snow",
+				"sunny", "tstorms",
+				"nt_chanceflurries", "nt_flurries", "nt_chancesnow",
+				"nt_snow", "nt_chancerain",
+				"nt_rain", "nt_chancesleet", "nt_sleet",
+				"nt_chancetstorms", "nt_tstorms",
+				"nt_clear", "nt_sunny", "nt_cloudy",
+				"nt_fog", "nt_hazy",
+				"nt_mostlycloudy", "nt_partlycloudy",
+				"nt_mostlysunny", "nt_partlysunny"
+			]
+		},
+		"wind_kph": [0, 15],
+		"feelslike_c": [-30, 50],
+		"precip_today_metric": [0, 100],
+		"celsius": [-20.0, 40.0]
+	};
 
-var __evaluate = function(o) {
-	if (!o) {
-		return;
-	}
+	this.rand = function(min, max) {
+		if (!max) {
+			max = min;
+			min = 0;
+		}
+		return min + Math.floor((max - min) * Math.random());
+	};
 
-	if (o.length === 2) { //contains two boundaries
-		return rand(o[0], o[1]);
-	} else { //expect an object
-		var range = o.range;
-		var select = o.select;
-		var prefix = o.prefix || "";
-		var suffix = o.suffix || "";
+	this.__evaluate = function(field, o) {
+		if (!o) {
+			return;
+		}
 
 		var result = "";
+		if (o.length === 2) { //contains two boundaries
+			result = that.rand(o[0], o[1]);
+		} else { //expect an object
+			if(!!o.depends && o.depends.field){
+				//resolve dependency if it does not exist yet:
+				if(!that.stack[o.depends.field]){
+					that.__evaluate(o.depends.field, that.fields[o.depends.field]);
+				}
 
-		result += prefix;
-		result += range ? rand(range[0], range[1]) : "";
-		result += select ? select[rand(select.length-1)] : "";
-		result += suffix;
+				//has a dependency
+				if(o.depends.fn){
+					result = o.depends.fn(o.depends.field, that.stack[o.depends.field]);
+				}else{
+					result = that.stack[o.depends.field];
+				}
+			}else{
+				var range = o.range;
+				var select = o.select;
+				var prefix = o.prefix || "";
+				var suffix = o.suffix || "";
+
+				result += prefix;
+				result += range ? that.rand(range[0], range[1]) : "";
+				result += select ? select[that.rand(select.length-1)] : "";
+				result += suffix;
+			}
+		}
+
+		//add to stack
+		that.stack[field] = result;
+
 		return result;
-	}
-};
+	};
+	
+	this.randomize = function(k, v) {
+		if (k === "") {
+			return v;
+		}
+		var val = null;
+		if (that.fields.hasOwnProperty(k)) {
+			val = that.__evaluate(k, that.fields[k]);
+		}
+		return val || v;
+	};
 
-module.exports = function(k, v) {
-	if (k === "") {
-		return v;
-	}
-	var val = null;
-	if (fields.hasOwnProperty(k)) {
-		val = __evaluate(fields[k]);
-	}
-	return val || v;
+	return this;
 };
