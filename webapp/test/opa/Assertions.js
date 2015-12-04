202e091296dd005c682ee8e0137ecd1c6cae68c0
Tanja Weiser <tanja.weiser@sap.com>
2015-12-04 14:58:03 +0100

    OPA
    Change-Id: I8a9edf106bae03c9b712f8a99ff664799c265dc5

--- webapp/test/opa/Assertions.js
+++ webapp/test/opa/Assertions.js
@@ -28,6 +28,39 @@ hss.weather.test.opa.Assertions = new sap.ui.test.Opa5({
 		});
 	},
 
+	iDoNotSeeControl: function(id){
+		return this.waitFor({
+			pollingInterval : 100,
+			controlType: "sap.ui.core.Control",
+			check : function (aControls) {
+       			for(var i = 0; i < aControls.length; i++){
+       				if(new RegExp(id).test(aControls[i].getId()) && !aControls[i].getVisible()){
+       					return true;
+       				}
+       			}
+       			return false;
+    		},
+			success: function(oControl){
+				sap.ui.test.Opa5.assert.ok(true, "Did not find control");
+			},
+			errorMessage: "Found control"
+		});
+
+
+
+		// return this.waitFor({
+		// 	id: new RegExp(id),
+		// 	matchers: new sap.ui.test.matchers.Properties({
+		// 		visible: false
+		// 	}),
+		// 	success: function(oControl){
+		// 		oCtrl = oControl;
+		// 		sap.ui.test.Opa5.assert.ok(true, "Did not find matching control");
+		// 	},
+		// 	errorMessage: "Find matching control"
+		// });
+	},
+
 	/**
 	 * Checks if the image source matches
 	 * @param  {String} id       Id of the control
@@ -57,5 +90,21 @@ hss.weather.test.opa.Assertions = new sap.ui.test.Opa5({
 			},
 			errorMessage: "Did not find dialog with matching id (" + id  +")"
 		});
-	}
+	},
+
+	iSeeToastMessage: function(){
+		return this.waitFor({
+			pollingInterval : 100,
+			// viewName: viewName,
+			// controlType: controlType, //e.g sap.m.Label
+			check : function () {
+       			return !!sap.ui.test.Opa5.getJQuery()(".sapMMessageToast").length;
+    		},
+			success: function(oControl){
+				sap.ui.test.Opa5.assert.ok(true, "Found the control: (" + oControl + ")");
+			},
+			errorMessage: "Did not find the control"
+		});
+	},
+
 });
